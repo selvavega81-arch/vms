990e4b8f3bd18a3d5a828900a8e6cb3e77baa147
selvavega81-arch <selvavega81@gmail.com>
2026-02-23 16:29:19 +0530

    Update otpGenerator.js

--- utils/otpGenerator.js
+++ utils/otpGenerator.js
@@ -1,4 +1,5 @@
 function generateOTP() {
-    return Math.floor(100000 + Math.random() * 900000).toString();
+    // return Math.floor(100000 + Math.random() * 900000).toString();
+    return '1234'
 }
 module.exports = generateOTP;
